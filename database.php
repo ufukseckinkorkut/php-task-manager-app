4742f02a1a2641054f982cd1ddb790d4dab54c14
usk <uskn2004@gmail.com>
2025-05-20 11:08:03 +0300

    created database connection and dynamic login page

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php
+
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_password = "";
+    $db_name = "task_manager_db";
+    $conn = "";
+
+    $conn = mysqli_connect($db_server, $db_user, $db_password, $db_name);
+
+
+     /* if ($conn) {
+        echo "you are connected";
+    }else {
+        echo"NOPE";
+    } */
+
+?>
